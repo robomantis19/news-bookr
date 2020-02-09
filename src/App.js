93e94485338e6d213401bb7817e919f913fe68d4
Thomas <thomaskatalenas@Thomass-MacBook-Air.local>
2020-02-09 10:29:36 -0500

    added route to home and profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Home from './components/Home'; 
+import Home from './components/Home';
+import Profile from './components/Profile'; 
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom'; 
+import { PrivateRoute } from './helpers/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div className="App">e
+      <Route exact path='/' component={Home} />
+      <Route path='/profile' component={Profile} />
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+
+function Profile() {
+
+
+
+    return (
+        <div>
+
+            Welcome to the Profile
+        </div>
+    )
+}
+export default Profile;
