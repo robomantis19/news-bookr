385450f1c39c5163235c86f854c08a9195437b06
robomantis19 <freefavors@gmail.com>
2020-10-26 12:59:20 -0400

    added a modal

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,6 +8,9 @@ import {Link as HBLink} from 'react-router-dom';
 
 import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import SimpleModal from '@material-ui/core/Modal';
 
 
 // album
@@ -22,7 +25,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 //navbar
@@ -32,6 +35,22 @@ import Topbar from './Topbar.js'
 
 
 const bblue = orange[800];
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
 function Profile(props) { 
     const [input, setInput] = useState("")      //https://www.daveramsey.com/blog/real-estate-trends'})
     const [output, setOutput] = useState({}); 
@@ -180,6 +199,36 @@ function Copyright() {
   //     }];
  
   const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const useStyles2 = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      backgroundColor: 'white'
+    },
+  }));
+  const body = (
+    <div style={{backgroundColor: 'white', marginLeft: '10%', marginTop: '10%', width: '800px', height: '400px', marginBottom: '200px'}} className={classes.paper}>
+      <h2 id="simple-modal-title">Sign in</h2>
+      <p  id="simple-modal-description">
+        sign up
+      </p>
+      <SimpleModal />
+    </div>
+  );
     return (
         <div>
              
@@ -202,6 +251,17 @@ function Copyright() {
                         <Typography variant="h6" className={classes.menuText} color="inherit" noWrap>
                             Housing Busters
                         </Typography>
+                        <button type="button" onClick={handleOpen}>
+                          Open Modal
+                        </button>
+                        <Modal
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="simple-modal-title"
+                          aria-describedby="simple-modal-description"
+                        >
+                          {body}
+                        </Modal>
                         <Typography variant="h6"  className={classes.menuText} color="inherit" noWrap>
                         <HBLink  className="link-color" style={{textDecoration: 'none', color: 'white'}} to='/'> Home </HBLink>
                         </Typography>
